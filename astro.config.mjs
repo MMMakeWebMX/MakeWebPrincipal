983dbbf31a6f1ed5b28fc4578463e8523ef4cbcb
ninhoProgrammer <mariohm100293@gmail.com>
2025-05-09 14:09:07 -0600

    feat: add Vercel analytics integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-
+import vercel from '@astrojs/vercel/serverless';
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true, // set to false when using @vercel/analytics@1.4.0
+    },
+  }),
   integrations: [tailwind(), vue()]
 });
