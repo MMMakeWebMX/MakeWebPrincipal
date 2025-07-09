cf397389a566a509a6b01eb0f5e7b07f24927cab
ninhoProgrammer <mariohm100293@gmail.com>
2025-07-09 12:25:46 -0600

    Fix: vercel analitics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+//import vercel from '@astrojs/vercel/serverless';
 import vue from "@astrojs/vue";
-import vercelAnalytics from '@vercel/analytics/astro';
 
 export default defineConfig({
-  adapter: vercel(),
-  integrations: [tailwind(), vue(), vercelAnalytics()],
+  //adapter: vercel(),
+  integrations: [tailwind(), vue()],
 });
